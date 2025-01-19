c7278b58e0bb0f7da056de00e6b69a47d12a119b
Raghav Gupta <142162663+Raghav-56@users.noreply.github.com>
2025-01-19 14:04:57 +0000

    Customised Portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Raghav Gupta",
+  title: "Student, AI/ML Enthusiast and web developer",
+  email: "raghavrgupta56@gmail.com",
+  gitHub: "Raghav-56",
+  instagram: "https://www.instagram.com/raghavrgup56/",
+  linkedIn: "https://www.linkedin.com/in/raghav-gupta-035b4a292/",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "https://x.com/Raghav_rgup",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
